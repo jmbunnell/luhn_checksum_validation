e29d891c55878ec603235d8f0f5ef16c66980be0
JB <jacobbunnellca@gmail.com>
2022-10-21 11:34:40 -0400

    Pseudocode

---
+++ checksum.py
@@ -0,0 +1,7 @@
+# Using original number, double the value of every other digit ()
+
+# Then, add the values of the individual digits together (ie: 17, add 1 and 7 not 17)
+
+# Identification number is valid if the sum is divisible by 10 (ie: 1762483 = 30)
+
+#
