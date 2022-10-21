5004b08a633d6688863e0a26c16fe4b78ff728fc
JB <jacobbunnellca@gmail.com>
2022-10-21 11:53:37 -0400

    User input and doubles every other digit

--- checksum.py
+++ checksum.py
@@ -1,7 +1,28 @@
-# Using original number, double the value of every other digit ()
+# 
 
 # Then, add the values of the individual digits together (ie: 17, add 1 and 7 not 17)
 
-# Identification number is valid if the sum is divisible by 10 (ie: 1762483 = 30)
+# Identification number is valid if the sum is divisible by 10 (ie: 176248 = 30)
 
-#
+import math
+
+# Prompt user for a number
+input = input("Number: ")
+print(f"{input}")
+
+# Blank list to push new numbers
+new = []
+
+
+# Using input, double the value of every other digit
+count = 0
+for i in input:
+    count += 1
+    if count % 2 == 0:
+        j = int(i) * 2
+        new.append(j)
+    else:
+        new.append(int(i))
+    
+print(f"{new}")
+
